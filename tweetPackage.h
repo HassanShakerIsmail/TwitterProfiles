9b3a9491d31567b0609b49f35cf2783c1e061a43
Hassan <70728230+TacitusKilgore@users.noreply.github.com>
2022-04-24 12:53:05 -0400

    Add files via upload

--- tweetPackage.h
+++ tweetPackage.h
@@ -1,10 +1,16 @@
+#pragma once
+#include <vector>
+#include <sstream>
+#include "personality.cpp"
+#include <iostream>
+
 #ifndef CALCULATIONS_H
 #define CALCULATIONS_H
 
 #include <string>
 
 class TweetPackage {
-    std::string tweet;
+	std::string tweet;
 	int likes;
 	int replies;
 	int retweets;
@@ -14,28 +20,31 @@ class TweetPackage {
 	int authorFollowers;
 	int authorFollowing;
 
-    public: 
-        TweetPackage(); 
-        TweetPackage(std::string tweet, int likes, int replies, int retweets, int quotes, std::string authorName, std::string authorUsername, int authorFollowers, int authorFollowing); 
-        std::string getTweet();
-        int getLikes();
-        int getReplies();
-        int getRetweets();
-        int getQuotes();
-        std::string getAuthorName();
-        std::string getAuthorUsername();
-        int getAuthorFollowers();
-        int getAuthorFollowing();
-        void setTweet(std::string Tweet);
-        void setLikes(int Likes);
-        void setReplies(int Replies);
-        void setRetweets(int Retweets);
-        void setQuotes(int Quotes);
-        void setAuthorName(std::string authorName);
-        void setAuthorUsername(std::string authorUsername);
-        void setAuthorFollowers(int authorFollowers);
-        void setAuthorFollowing(int authorFollowing);
-}; 
+public:
+	TweetPackage();
+	TweetPackage(std::string tweet, int likes, int replies, int retweets, int quotes, std::string authorName, std::string authorUsername, int authorFollowers, int authorFollowing);
+	std::string getTweet();
+	int getLikes();
+	int getReplies();
+	int getRetweets();
+	int getQuotes();
+	std::string getAuthorName();
+	std::string getAuthorUsername();
+	int getAuthorFollowers();
+	int getAuthorFollowing();
+	void setTweet(std::string Tweet);
+	void setLikes(int Likes);
+	void setReplies(int Replies);
+	void setRetweets(int Retweets);
+	void setQuotes(int Quotes);
+	void setAuthorName(std::string authorName);
+	void setAuthorUsername(std::string authorUsername);
+	void setAuthorFollowers(int authorFollowers);
+	void setAuthorFollowing(int authorFollowing);
+	bool isValidTweetPackage();
+};
 
 
 #endif 
+
+
